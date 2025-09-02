174efe604f465ac9a3983851743fb8533d434a03
Lucas <lucaspassos11@hotmail.com>
2025-09-02 14:36:33 -0300

    first commit

---
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/products")
+    public String getProducts() {
+        return "Lista de produtos";
+    }
+}
